3186aa1c72699edd8770d71871923c49960c9401
Kajaqq <kajos200@gmail.com>
2025-02-02 20:11:04 +0100

    Update files, tidy up the csv/json bridge.

--- scripts/csv_json_bridge.py
+++ scripts/csv_json_bridge.py
@@ -28,28 +28,6 @@ def read_json(filepath):
     except json.JSONDecodeError:
         raise json.JSONDecodeError(f"Invalid JSON format in {filepath}")
 
-def write_csv(filepath, data:dict, header=None):
-    """Writes data to a CSV file.
-
-    Args:
-        filepath (str): The path to the CSV file.
-        data (dict): A list of dictionaries representing the rows.
-        header (list): A list of strings representing the header.
-
-    Raises:
-        IOError: If there is an error writing to the file.
-    """
-    if header is None:
-        header = CSV_HEADER
-    try:
-        with open(filepath, 'w', newline='', encoding='utf-8') as csv_data:
-            writer = csv.writer(csv_data)
-            writer.writerow(header)
-            for key, value in data.items():
-                writer.writerow([key, value])
-    except IOError as e:
-        raise IOError(f"Error writing to CSV file {filepath}: {e}")
-
 def read_csv(filepath):
     """Reads a CSV file and returns its content as a list of dictionaries.
 
@@ -88,6 +66,29 @@ def write_json(filepath, data):
     except IOError as e:
         raise IOError(f"Error writing to JSON file {filepath}: {e}")
 
+def write_csv(filepath, data:dict, header=None):
+    """Writes data to a CSV file.
+
+    Args:
+        filepath (str): The path to the CSV file.
+        data (dict): A list of dictionaries representing the rows.
+        header (list): A list of strings representing the header.
+
+    Raises:
+        IOError: If there is an error writing to the file.
+    """
+    if header is None:
+        header = CSV_HEADER
+    try:
+        with open(filepath, 'w', newline='', encoding='utf-8') as csv_data:
+            writer = csv.writer(csv_data)
+            writer.writerow(header)
+            writer = csv.writer(csv_data, quoting=csv.QUOTE_ALL)
+            for key, value in data.items():
+                writer.writerow([key, value])
+    except IOError as e:
+        raise IOError(f"Error writing to CSV file {filepath}: {e}")
+
 def json_to_csv(json_filepath, csv_filepath):
     """Converts a JSON file to a CSV file, handling empty values.
 
@@ -109,10 +110,11 @@ def csv_to_json(csv_filepath, json_filepath):
     data = read_csv(csv_filepath)
     json_data = {}
     for row in data:
+        source_row = row['source']
         if 'translatedstr' in row:
-            json_data[row['source']] = row['translatedstr']
+            json_data[source_row] = row['translatedstr']
         else:
-            json_data[row['source']] = ''  # Handle missing 'translatedstr' as empty string
+            json_data[source_row] = ''  # Handle missing 'translatedstr' as empty string
     write_json(json_filepath, json_data)
 
 def mass_csv_to_json(csv_folder, json_folder=None):
